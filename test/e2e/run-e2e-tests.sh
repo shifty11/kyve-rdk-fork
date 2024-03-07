25f247f26d4d54a75c1d74b9ecfd7e56efd1e5ed
rapha <raphael.thurnherr1990@gmail.com>
2024-03-07 16:45:21 +0100

    fix: run-e2e-tests.sh volume mount

--- test/e2e/run-e2e-tests.sh
+++ test/e2e/run-e2e-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-E2ETEST_DIR=./test/e2e
+E2ETEST_DIR=${PWD}/test/e2e
 
 # Capture the first argument which will be passed to the e2e test
 arg="$1"
@@ -29,9 +29,9 @@ docker run \
     --privileged                             `# Run in privileged mode` \
     -d                                       `# Run in detached mode` \
     --name e2etest                           `# Name the container` \
-    -v ./runtime:/mnt/runtime:ro             `# Mount runtime folder` \
-    -v ./tools:/tools:ro                     `# Mount tools folder` \
-    -v ./protocol:/protocol:ro               `# Mount protocol folder` \
+    -v ${PWD}/runtime:/mnt/runtime:ro        `# Mount runtime folder` \
+    -v ${PWD}/tools:/tools:ro                `# Mount tools folder` \
+    -v ${PWD}/protocol:/protocol:ro          `# Mount protocol folder` \
     -v e2etestvol:/var/lib/docker            `# Mount docker volume (to cache images)` \
     e2etest
 
