86a35891a71161517a804c2c2f49fb8f3e93e703
rapha <raphael.thurnherr1990@gmail.com>
2024-03-12 15:10:17 +0100

    feat: add super fancy go runtime

---
+++ runtime/MySuperFancyGoRuntime/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/KYVENetwork/kyve-rdk/runtime/MySuperFancyGoRuntime/server"
+)
+
+func main() {
+	server.StartServer()
+}
---
+++ runtime/MySuperFancyGoRuntime/utils/utils.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func GetJsonFromUrl(url string) (map[string]interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	//goland:noinspection ALL
+	defer resp.Body.Close()
+
+	var data map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func GetFromUrl(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	//goland:noinspection ALL
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
